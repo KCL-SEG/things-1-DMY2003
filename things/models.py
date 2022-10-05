7a1b902d0ee702ade8ee3b21e848b6bfc4004c76
Damian Mitrofan <damian.mitrofan@gmail.com>
2022-10-05 15:36:58 +0100

    I am getting tired of this

--- things/models.py
+++ things/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
-    name = models.CharField(max_length =30, blank = True, unique=True)
+    name = models.CharField(max_length =30, blank = False, unique=True)
     description = models.CharField(max_length =120)
     quantity = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
